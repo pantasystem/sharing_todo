f8e969be30f4d8c41a3572b8fa70210fce62f31f
kinoshita0623 <kuutarousii@gmail.com>
2020-07-29 16:02:59 +0900

    TodoのベースとなるTopicのミグレーションを作成した

---
+++ database/migrations/2020_07_29_070036_create_topics_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTopicsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('topics', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->timestamps();
+            $table->bigInteger('author_id');
+            $table->bigInteger('group_id');
+            $table->string('title');
+            $table->string('description')->nullable();
+            $table->foreign('author_id')
+                ->references('id')->on('users')
+                ->onDelete('cascade')
+                ->onUpdate('cascade');
+            $table->foreign('group_id')
+                ->references('id')->on('groups')
+                ->onDelete('cascade')
+                ->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('topics');
+    }
+}
