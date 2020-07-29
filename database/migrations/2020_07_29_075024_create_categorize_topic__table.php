f8be5922a2507f78f23cbfec022328ee7aa9ea2b
kinoshita0623 <kuutarousii@gmail.com>
2020-07-29 16:50:33 +0900

    カテゴリとTodoをマップするmigrationを作成した

--- database/migrations/2020_07_29_075024_create_categorize_topic__table.php
+++ database/migrations/2020_07_29_075024_create_categorize_topic__table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateCategorizeTopicTable.php extends Migration
+class CreateCategorizeTopicTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,10 @@ class CreateCategorizeTopicTable.php extends Migration
      */
     public function up()
     {
-        //
+        Schema::create('categorize_topic_', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->timestamps();
+        });
     }
 
     /**
@@ -23,6 +26,6 @@ class CreateCategorizeTopicTable.php extends Migration
      */
     public function down()
     {
-        //
+        Schema::dropIfExists('categorize_topic_');
     }
 }
