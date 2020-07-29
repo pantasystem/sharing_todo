6fe5c434b27f7c331edd5e3b092b72d70170368e
kinoshita0623 <kuutarousii@gmail.com>
2020-07-29 13:16:00 +0900

    MessageとGroupの関連を実装した

--- app/Message.php
+++ app/Message.php
@@ -3,6 +3,7 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Group;
 
 class Message extends Model
 {
@@ -10,7 +11,7 @@ class Message extends Model
 
     public function group()
     {
-        
+        return $this->belongsTo(Grouop::class);
     }
 
     public function author()
