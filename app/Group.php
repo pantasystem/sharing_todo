e5dcb4e17c168ebf4b90b1e542aeb26e7188e83a
kinoshita0623 <kuutarousii@gmail.com>
2020-07-29 13:06:46 +0900

    GroupにMessageの関連を実装した

--- app/Group.php
+++ app/Group.php
@@ -12,6 +12,10 @@ class Group extends Model
     //
     public function members()
     {
+        //
+        // 第二引数結合テーブル名
+        // 第３引数はリレーションを定義しているモデルの外部キー名で、
+        // 一方の第４引数には結合するモデルの外部キー名を渡します。
         return $this->belongsToMany(User::class, 'members', 'user_id', 'group_id');
 
     }
@@ -19,10 +23,11 @@ class Group extends Model
     public function todos()
     {
         //return $this->hasMany(Todo:class);
+        return $this->hasMany(Todo::class);
     }
 
     public function messages()
     {
-        
+        return $this->hasMany(Message::class);
     }
 }
