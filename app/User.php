1d56646010198c20672be5ead4918cbcc404ec9e
kinoshita0623 <kuutarousii@gmail.com>
2020-08-01 02:38:04 +0900

    データベースのテストはFeatureですることが推奨されているようだ

--- app/User.php
+++ app/User.php
@@ -30,7 +30,7 @@ class User extends Authenticatable
 
     public function todos()
     {
-        return $this->hasMany(Todo::class);
+        return $this->hasMany(Todo::class, 'author_id');
     }
 
     public function comments()
--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -3,9 +3,13 @@
 namespace Tests\Unit;
 
 use PHPUnit\Framework\TestCase;
+use App\User;
+use App\Group;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class ExampleTest extends TestCase
 {
+    use RefreshDatabase;
     /**
      * A basic test example.
      *
@@ -13,6 +17,18 @@ class ExampleTest extends TestCase
      */
     public function testBasicTest()
     {
+        //$this->assertTrue(true);
+        /*$userData = [
+            'name' => 'AName',
+            'email' => 'aemail@email.jp',
+            'password' => 'secret'
+        ];
+
+        $user = new User();
+        $user->fill($userData)->save();*/
+        //$this->assertDatabaseHas('users', $userData);
         $this->assertTrue(true);
+        //$result = DB::table('users')->find(1);
+        //$this->assertTrue($result);
     }
 }
