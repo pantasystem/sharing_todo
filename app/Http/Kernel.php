ff8138a06535d066995f7a42bddc7682f7d1035e
kinoshita0623 <kuutarousii@gmail.com>
2020-08-02 12:00:15 +0900

    APIでもセッション認証ができるようにした

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -46,6 +46,7 @@ class Kernel extends HttpKernel
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
             'throttle:60,1',
+            'bindings',
         ],
     ];
 
