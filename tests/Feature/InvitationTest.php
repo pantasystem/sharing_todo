d8e0a08f74f002cd2f5b825a4e51ff8dd4cb9748
kinoshita0623 <kuutarousii@gmail.com>
2020-08-02 19:38:05 +0900

    有効期限に対してのテストケースを追加

--- tests/Feature/InvitationTest.php
+++ tests/Feature/InvitationTest.php
@@ -14,6 +14,11 @@ use Carbon\Carbon;
 
 class InvitationTest extends TestCase
 {
+
+    private $group;
+    private $targetUser;
+    private $groupMember;
+
     public function setUp(): void
     {
       parent::setUp();
@@ -21,24 +26,90 @@ class InvitationTest extends TestCase
       Artisan::call('migrate:fresh');
   
       $this->seed('DatabaseSeeder');
+
+      $this->group = Group::find(1);
+
+      $this->groupMember = $this->group->members()->first();
+
+      $this->targetUser = User::where('id', '<>', $this->groupMember->id)->first();
+
   
     }
 
     public function testMakeInvitation()
     {
-        $group = Group::find(1);
-        $groupMember = $group->members()->first();
 
-        $targetUser = User::where('id', '<>', $groupMember->id)->first();
         
-        $invitation = $group->invitations()->create([
-            'author_id' => $groupMember->id,
-            'invitation_user_id' => $targetUser->id,
+        $invitation = $this->group->invitations()->create([
+            'author_id' => $this->groupMember->id,
+            'invitation_user_id' => $this->targetUser->id,
             'expiration_date' => null,
             'is_accept' => null,
         ]);
 
         $this->assertNotNull($invitation);
-        $this->assertNotNull($targetUser->invitations()->active(Carbon::now())->first());
+        
+    }
+
+    public function testScopeActive()
+    {
+        $invitation = $this->group->invitations()->create([
+            'author_id' => $this->groupMember->id,
+            'invitation_user_id' => $this->targetUser->id,
+            'expiration_date' => null,
+            'is_accept' => null,
+        ]);
+
+        $this->assertNotNull($this->targetUser->invitations()->active(Carbon::now())->first());
+    }
+
+    public function testScopeActiveCaseAcceptTrue()
+    {
+        $invitation = $this->group->invitations()->create([
+            'author_id' => $this->groupMember->id,
+            'invitation_user_id' => $this->targetUser->id,
+            'expiration_date' => null,
+            'is_accept' => true,
+        ]);
+        $this->assertNull($this->targetUser->invitations()->active(Carbon::now())->first());
+
+    }
+
+    public function testScopeActiveCaseAcceptFalse()
+    {
+        $invitation = $this->group->invitations()->create([
+            'author_id' => $this->groupMember->id,
+            'invitation_user_id' => $this->targetUser->id,
+            'expiration_date' => null,
+            'is_accept' => false,
+        ]);
+        $this->assertNull($this->targetUser->invitations()->active(Carbon::now())->first());
+    }
+
+    public function testScopeActiveCaseExpitationDate()
+    {
+        $expitationDate = Carbon::now();
+        $expitationDate->addDay();
+        $invitation = $this->group->invitations()->create([
+            'author_id' => $this->groupMember->id,
+            'invitation_user_id' => $this->targetUser->id,
+            'expiration_date' => $expitationDate,
+            'is_accept' => false,
+        ]);
+        $this->assertNotNull($this->targetUser->invitations()->active(Carbon::now())->first());
+
+    }
+
+    public function testScopeActiveCaseExpired()
+    {
+        $expitationDate = Carbon::now();
+        $expitationDate->subDay();
+        $invitation = $this->group->invitations()->create([
+            'author_id' => $this->groupMember->id,
+            'invitation_user_id' => $this->targetUser->id,
+            'expiration_date' => $expitationDate,
+            'is_accept' => false,
+        ]);
+        $this->assertNull($this->targetUser->invitations()->active(Carbon::now())->first());
     }
 }
